28456028bf0440e5d10d3acad2b94da673b0fbb3
Gil Collins <50967180+colt3k@users.noreply.github.com>
2021-06-03 11:03:13 -0700

    update debug logging

--- updater/artifactory/updater.go
+++ updater/artifactory/updater.go
@@ -32,16 +32,17 @@ import (
 var ac *updater.AppConfig
 
 func CheckUpdate(appName string, hosts []updater.Connection, version updater.Version) (*updater.AppConfig, bool, bool) {
-
+	test := false
 	testHosts(hosts)
+	log.Logf(log.DEBUG, "- CheckUpdate")
 	for _, d := range hosts {
-
 		var autoUpdate bool
 
 		// if all checks failed skip
 		if !d.Available() && !d.HostPfx() && !d.HostSuffix() {
 			continue
 		}
+		log.Logf(log.DEBUG, "-- attempt host %v", d.OnAvailable)
 		var base bytes.Buffer
 		var upUrl bytes.Buffer
 		var autoUrl bytes.Buffer
@@ -54,18 +55,26 @@ func CheckUpdate(appName string, hosts []updater.Connection, version updater.Ver
 		base.WriteString(d.Path)
 
 		upUrl.WriteString(base.String())
-		upUrl.WriteString(appName + "-" + runtime.GOOS + "-" + runtime.GOARCH + ".update")
-		log.Logln(log.DEBUG, "URL:", upUrl.String())
+		if !test {
+			upUrl.WriteString(appName + "-" + runtime.GOOS + "-" + runtime.GOARCH + ".update")
+		} else {
+			upUrl.WriteString(appName + "-linux-" + runtime.GOARCH + ".update")
+		}
+		log.Logf(log.DEBUG, "-- Update File URL: %v", upUrl.String())
 
 		var updateAvailable bool
 		var compressdSuffix string
 		url := upUrl.String()
-		compressdSuffix = "-" + runtime.GOOS + "-" + runtime.GOARCH + ".tgz"
+		if !test {
+			compressdSuffix = "-" + runtime.GOOS + "-" + runtime.GOARCH + ".tgz"
+		} else {
+			compressdSuffix = "-linux-" + runtime.GOARCH + ".tgz"
+		}
 
 		auth := &hc.Auth{Username: user, Password: pass}
 		data, err := pullURLToString(url, auth, d.DisableValidateCert)
-		if bserr.WarnErr(err) {
-			log.Logf(log.WARN, "update site unreachable %v", err.Error())
+		if err != nil {
+			log.Logf(log.WARN, "--- %v", err.Error())
 			//return nil, updateAvailable
 			continue
 		}
@@ -77,39 +86,39 @@ func CheckUpdate(appName string, hosts []updater.Connection, version updater.Ver
 		ac.DisableVerifyCert = d.DisableValidateCert
 
 		dec := json.NewDecoder(ioutil.NopCloser(strings.NewReader(data)))
-		if err := dec.Decode(&ac); bserr.Err(err, "error decoding") {
+		if err = dec.Decode(&ac); bserr.Err(err, "error decoding") {
 			return ac, updateAvailable, autoUpdate
 		}
 
 		// Check for Auto file and value
 		autoUrl.WriteString(base.String())
 		autoUrl.WriteString(appName + ".auto")
-		log.Logln(log.DEBUG, "URL:", autoUrl.String())
+		log.Logf(log.DEBUG, "-- Auto File URL: %v", autoUrl.String())
 		autoURI := autoUrl.String()
 		autoDat, err := pullURLToString(autoURI, auth, d.DisableValidateCert)
-		if bserr.WarnErr(err) {
-			log.Logf(log.WARN, "no auto file available %v", err.Error())
+		if err != nil {
+			log.Logf(log.WARN, "--- %v", err.Error())
 		}
 		if len(autoDat) > 0 {
 			var errAU error
 			autoUpdate, errAU = strconv.ParseBool(autoDat)
 			if errAU != nil {
-				log.Logf(log.ERROR, "issue parsing auto update file %v", errAU)
+				log.Logf(log.ERROR, "--- issue parsing auto update file %v", errAU)
 			}
 		}
 
 		curVer, err := semver.Make(strings.TrimPrefix(version.Version, "v"))
 		xVer, err := semver.Make(strings.TrimPrefix(ac.Version, "v"))
 
-		log.Logf(log.DEBUG, "Current Version: %s, Remote Version: %s", curVer.String(), xVer.String())
+		log.Logf(log.DEBUG, "-- Current Version: %s, Remote Version: %s", curVer.String(), xVer.String())
 		remoteTime := time.Unix(ac.Timestamp, 0)
 		unx, _ := strconv.ParseInt(version.BuildDate, 10, 64)
 		localTime := time.Unix(unx, 0)
-		log.Logf(log.DEBUG, "LOCAL  App Name: %s, OS: %s/%s, Version: %s, App Time: %v, Converted: %v", appName, runtime.GOOS, runtime.GOARCH, version.Version, version.BuildDate, localTime)
-		log.Logf(log.DEBUG, "REMOTE App Name: %s, OS: %s/%s, Version: %s, App Time: %v, Converted: %v", appName, ac.OS, ac.Arch, ac.Version, ac.Timestamp, remoteTime)
+		log.Logf(log.DEBUG, "-- LOCAL  App Name: %s, OS: %s/%s, Version: %s, App Time: %v, Converted: %v", appName, runtime.GOOS, runtime.GOARCH, version.Version, version.BuildDate, localTime)
+		log.Logf(log.DEBUG, "-- REMOTE App Name: %s, OS: %s/%s, Version: %s, App Time: %v, Converted: %v", appName, ac.OS, ac.Arch, ac.Version, ac.Timestamp, remoteTime)
 
 		if xVer.GT(curVer) {
-			log.Logln(log.DEBUG, "remote version is newer")
+			log.Logf(log.DEBUG, "-- remote version is newer %v > %v", xVer, curVer)
 			base.WriteString(appName + compressdSuffix)
 			ac.URL = base.String()
 			ac.ArchiveName = appName + compressdSuffix
@@ -118,21 +127,21 @@ func CheckUpdate(appName string, hosts []updater.Connection, version updater.Ver
 
 		} else if localTime.Before(remoteTime) { // if current app is older than remote pull, could be a roll back
 			//Check build time instead
-			log.Logln(log.DEBUG, "remote time is newer than local")
+			log.Logln(log.DEBUG, "-- remote time is newer than local %v > %v", remoteTime, localTime)
 			base.WriteString(appName + compressdSuffix)
 			ac.URL = base.String()
 			ac.ArchiveName = appName + compressdSuffix
 			if ac.Name == appName && ac.OS == runtime.GOOS {
-				log.Logln(log.DEBUG, "update available!")
+				log.Logln(log.DEBUG, "-- update available!")
 				updateAvailable = true
 				return ac, updateAvailable, autoUpdate
 			}
 		} else {
-			log.Logln(log.DEBUG, "local version is newer")
+			log.Logln(log.DEBUG, "-- local version is newer")
 		}
 		return ac, updateAvailable, autoUpdate
 	}
-
+	log.Logln(log.DEBUG, "- CheckUpdate END")
 	return nil, false, false
 }
 
@@ -161,7 +170,7 @@ func PerformUpdate(appName string, hosts []updater.Connection, version updater.V
 		2. Place in proper location (same as normal install)
 		3. exit application and notify to restart - due to update or make option via input
 	*/
-	log.Logln(log.DEBUG, "check for update")
+	log.Logln(log.DEBUG, "- Perform Update")
 	if ac, found, autoUpdate := CheckUpdate(appName, hosts, version); found {
 		s := UpdateAvailableMsg()
 		fmt.Println(s)
@@ -173,14 +182,18 @@ func PerformUpdate(appName string, hosts []updater.Connection, version updater.V
 			return found
 		}
 	}
+	log.Logln(log.DEBUG, "- Perform Update END")
 	return false
 }
 
 func downloadUpdate(ac *updater.AppConfig) {
+	log.Logln(log.DEBUG, "- Download Update")
 	//Download
 	if download(ac) {
 		// success
+		log.DisableTimestamp()
 		log.Println("\n** successful download, exiting so you can restart the application **")
+		log.EnableTimestamp()
 		os.Exit(0)
 	} else {
 		// failed
@@ -188,12 +201,14 @@ func downloadUpdate(ac *updater.AppConfig) {
 		log.Printf("\nupdate failed %v", ac.Issue)
 		log.EnableTimestamp()
 	}
+	log.Logln(log.DEBUG, "- Download Update END")
 }
 
 func pullChangeLogAndDisplay(ac *updater.AppConfig) string {
 	if ac != nil {
-		log.Println("Chg: ", ac.Changelog)
-		log.Println("URL:", ac.BaseURL)
+		log.Logln(log.DEBUG, "- Pull Change Log and Display")
+		log.Logf(log.DEBUG,"-- change log: %v", ac.Changelog)
+		log.Logf(log.DEBUG,"-- url used: %v", ac.BaseURL)
 		if len(ac.Changelog) > 0 {
 			httpClient := hc.NewClient(hc.HttpClientRequestTimeout(20), hc.DisableVerifyClientCert(ac.DisableVerifyCert))
 			var err error
@@ -203,14 +218,14 @@ func pullChangeLogAndDisplay(ac *updater.AppConfig) string {
 			if resp != nil {
 				defer resp.Body.Close()
 			}
-			if bserr.WarnErr(err) {
-				log.Logf(log.WARN, "update site unreachable %v", err.Error())
+			if err != nil {
+				log.Logf(log.WARN, "--- %v", err.Error())
 			}
 
 			// Read body to buffer
 			body, err := ioutil.ReadAll(resp.Body)
 			if bserr.Err(err, "Error reading body") {
-				log.Logln(log.WARN, "error reading body on changelog")
+				log.Logln(log.WARN, "--- error reading body on changelog")
 			}
 			var byt bytes.Buffer
 			lines := strings.SplitAfter(string(body), "\n")
@@ -221,11 +236,12 @@ func pullChangeLogAndDisplay(ac *updater.AppConfig) string {
 			return byt.String()
 		}
 	}
+	log.Logln(log.DEBUG, "- Pull Change Log and Display END")
 	return ""
 }
 
 func download(ac *updater.AppConfig) bool {
-
+	log.Logln(log.DEBUG, "- Download")
 	var success bool
 	httpClient := hc.NewClient(hc.HttpClientRequestTimeout(20), hc.DisableVerifyClientCert(ac.DisableVerifyCert))
 
@@ -234,34 +250,34 @@ func download(ac *updater.AppConfig) bool {
 	if resp != nil {
 		defer resp.Body.Close()
 	}
-	if bserr.WarnErr(err) {
-		log.Logf(log.ERROR, "update site unreachable %v", err.Error())
+	if err != nil {
+		log.Logf(log.ERROR, "--- %v", err.Error())
 		ac.Issue = "update site unreachable"
 		return success
 	}
 
 	// Read body to buffer
 	body, err := ioutil.ReadAll(resp.Body)
-	if bserr.Err(err, "Error reading body") {
+	if bserr.Err(err, "--- error reading body") {
 		ac.Issue = "unable to read response"
 		return success
 	}
 
 	// write out
-	log.Logln(log.DEBUG, "Writing out update to ", ac.ArchiveName)
+	log.Logf(log.DEBUG, "-- writing out update to %v", ac.ArchiveName)
 	_, err = iout.WriteOut(body, ac.ArchiveName)
 	if err != nil {
-		log.Logf(log.ERROR, "issue writing out %v, %v", ac.ArchiveName, err)
+		log.Logf(log.ERROR, "--- issue writing out %v, %v", ac.ArchiveName, err)
 		ac.Issue = "unable to write out archive"
 		return success
 	}
 
 	// Pull executable from archive 'tgz'
-	log.Logln(log.DEBUG, "Extracting executable ", ac.Name)
+	log.Logf(log.DEBUG, "-- extracting executable %v", ac.Name)
 	cmd := "tar xvf " + ac.ArchiveName + " " + strings.TrimSuffix(ac.ArchiveName, ".tgz") + "/" + ac.Name
 	_, err = exec.Command("sh", "-c", cmd).Output()
 	if err != nil {
-		log.Logln(log.WARN, fmt.Sprintf("Failed to execute command: %s %s", cmd, err.Error()))
+		log.Logln(log.WARN, fmt.Sprintf("--- failed to execute command: %s %s", cmd, err.Error()))
 		ac.Issue = "failed to extract"
 		return success
 	}
@@ -271,7 +287,7 @@ func download(ac *updater.AppConfig) bool {
 	sha256HashFileName := ac.Name + ".sha256"
 	validHash := validateHash(sha256HashFileName, archivePathDir, ac.Name)
 	if !validHash {
-		log.Logln(log.INFO, "sha256 hash invalid")
+		log.Logln(log.INFO, "--- sha256 hash invalid")
 		ac.Issue = "invalid sha256 hash"
 		return success
 	}
@@ -283,14 +299,14 @@ func download(ac *updater.AppConfig) bool {
 		return success
 	}
 	if strings.HasSuffix(s, "main") {
-		log.Logln(log.WARN, "not a packaged executable")
+		log.Logln(log.WARN, "--- not a packaged executable")
 		ac.Issue = "not a packaged executable"
 		return success
 	}
 	cmd = "mv " + strings.TrimSuffix(ac.ArchiveName, ".tgz") + "/" + ac.Name + " " + s
 	_, err = exec.Command("sh", "-c", cmd).Output()
 	if err != nil {
-		log.Logln(log.WARN, fmt.Sprintf("Failed to execute command: %s %s", cmd, err.Error()))
+		log.Logln(log.WARN, fmt.Sprintf("--- failed to execute command: %s %s", cmd, err.Error()))
 		ac.Issue = "failed to move/replace application"
 		return success
 	}
@@ -299,19 +315,20 @@ func download(ac *updater.AppConfig) bool {
 	cmd = "rm -rf " + strings.TrimSuffix(ac.ArchiveName, ".tgz") + "*"
 	_, err = exec.Command("sh", "-c", cmd).Output()
 	if err != nil {
-		log.Logln(log.WARN, fmt.Sprintf("Failed to execute command: %s %s", cmd, err.Error()))
+		log.Logln(log.WARN, fmt.Sprintf("--- failed to execute command: %s %s", cmd, err.Error()))
 		ac.Issue = "failed to clean archive"
 		return success
 	}
 
 	success = true
+	log.Logln(log.DEBUG, "- Download END")
 	return success
 }
 
 func testHosts(hosts []updater.Connection) {
-	log.Logln(log.DEBUG, "test hosts")
+	log.Logln(log.DEBUG, "- test hosts")
 	host := osut.Hostname()
-	log.Logln(log.DEBUG, "local host", host)
+	log.Logf(log.DEBUG, "-- local host %v", host)
 	for i, d := range hosts {
 		// if neither is set skip entry
 		if len(d.OnAvailable) > 0 {
@@ -319,17 +336,17 @@ func testHosts(hosts []updater.Connection) {
 			var avail bool
 			var err error
 			if d.OnAvailableViaHTTP {
-				log.Logln(log.DEBUG, "onAvailable check", d.OnAvailable)
+				log.Logf(log.DEBUG, "-- onAvailable check %v", d.OnAvailable)
 				avail, err = hc.Reachable(d.OnAvailable, d.Name, 2, d.DisableValidateCert)
 			} else {
-				log.Logln(log.DEBUG, "ping host check", d.OnAvailable)
+				log.Logf(log.DEBUG, "-- ping host check %v", d.OnAvailable)
 				avail, err = netut.Ping(d.OnAvailable)
 			}
 			if err != nil {
-				log.Logf(log.DEBUG, err.Error())
+				log.Logf(log.DEBUG, "--- %v",err.Error())
 			}
 
-			log.Logf(log.DEBUG, "host available? %v: %s", avail, d.OnAvailable)
+			log.Logf(log.DEBUG, "--- host available? %v: %s", avail, d.OnAvailable)
 			hosts[i].SetAvailable(avail)
 			//if avail {
 			//	break
@@ -337,18 +354,18 @@ func testHosts(hosts []updater.Connection) {
 		}
 		// if OnAvailable is set and OnHostname is NOT set but host is not resolvable skip
 		if len(d.OnHostNamePrefix) > 0 && strings.HasPrefix(strings.ToLower(host), d.OnHostNamePrefix) {
-			log.Logf(log.DEBUG, "on host starting with: %s hostname %s", d.OnHostNamePrefix, host)
+			log.Logf(log.DEBUG, "--- on host starting with: %s hostname %s", d.OnHostNamePrefix, host)
 			hosts[i].SetHostPfx(true)
 		}
 		if len(d.OnHostNameSuffix) > 0 && strings.HasSuffix(strings.ToLower(host), d.OnHostNameSuffix) {
-			log.Logf(log.DEBUG, "on host ending with: %s hostname %s", d.OnHostNameSuffix, host)
+			log.Logf(log.DEBUG, "--- on host ending with: %s hostname %s", d.OnHostNameSuffix, host)
 			hosts[i].SetHostSfx(true)
 		}
 	}
 }
 
 func pullURLToString(url_ string, auth *hc.Auth, disableVerifyCert bool) (string, error) {
-	log.Logln(log.DEBUG, "obtain httpclient")
+	log.Logln(log.DEBUG, "--- Pull URL Content To String")
 	httpClient := hc.NewClient(hc.HttpClientRequestTimeout(20), hc.DisableVerifyClientCert(disableVerifyCert))
 
 	resp, err := httpClient.Fetch("GET", url_, auth, nil, nil)
@@ -385,18 +402,19 @@ func hash_file_md5(filePath string) (string, error) {
 
 }
 func validateHash(hashFileName, archivePathDir, appName string) bool {
-	log.Logln(log.DEBUG, "Extracting hash file ", hashFileName)
+	log.Logln(log.DEBUG, "- Validate Hash")
+	log.Logf(log.DEBUG, "-- extracting hash file %v", hashFileName)
 	cmd := "tar xvf " + ac.ArchiveName + " " + archivePathDir + "/" + hashFileName
 	_, err := exec.Command("sh", "-c", cmd).Output()
 	if err != nil {
-		log.Logln(log.WARN, fmt.Sprintf("Failed to execute command: %s %s", cmd, err.Error()))
+		log.Logln(log.WARN, fmt.Sprintf("--- failed to execute command: %s %s", cmd, err.Error()))
 		return false
 	}
 
 	// Read file contents
 	hashContent, err := ioutil.ReadFile(archivePathDir + "/" + hashFileName)
 	if err != nil {
-		log.Logln(log.WARN, fmt.Sprintf("Failed to read hash: %s", err.Error()))
+		log.Logln(log.WARN, fmt.Sprintf("--- failed to read hash: %s", err.Error()))
 		return false
 	}
 	// create hash from downloaded application
@@ -407,16 +425,17 @@ func validateHash(hashFileName, archivePathDir, appName string) bool {
 		hash, err = hash_file_sha256(archivePathDir + "/" + appName)
 	}
 	if err != nil {
-		log.Logln(log.WARN, fmt.Sprintf("Failed to create hash from application: %s", err.Error()))
+		log.Logln(log.WARN, fmt.Sprintf("--- failed to create hash from application: %s", err.Error()))
 		return false
 	}
 
 	if string(hashContent) == hash {
-		log.Logln(log.INFO, "VALID hash")
+		log.Logln(log.INFO, "-- VALID hash")
 		return true
 	} else {
-		log.Logf(log.INFO, "INVALID hash %s", hash)
+		log.Logf(log.INFO, "-- INVALID hash %s", hash)
 	}
+	log.Logln(log.DEBUG, "- Validate Hash END")
 	return false
 }
 func hash_file_sha256(filePath string) (string, error) {
