8aa88d956ced416c08591aee3ea04ee29054915e
Gil Collins <50967180+colt3k@users.noreply.github.com>
2021-08-11 11:42:15 -0700

    restructure

--- shutdown/shutdown.go
+++ shutdown/shutdown.go
@@ -4,20 +4,37 @@ import (
 	"context"
 	"fmt"
 	"os/signal"
+	"sync"
 	"syscall"
 )
 
-func Graceful(context context.Context, cleanup func()) context.Context {
-	ctx, stop := signal.NotifyContext(context,
-		syscall.SIGINT,  // interrupt: stopped by Ctrl + C
-		syscall.SIGTERM, // graceful kill
-	)
-	go func() {
-		s := <-ctx.Done()
-		fmt.Println("Got signal:", s)
-		fmt.Println("\r- Ctrl+C or (kill -9) received")
-		cleanup()
-		stop()
-	}()
-	return ctx
+var once sync.Once
+var (
+	instance *Graceful
+)
+
+type Graceful struct {
+	NotifyContext context.Context
+	stop          context.CancelFunc
+}
+
+func SetupNotifyContext(ctxt context.Context) *Graceful {
+	once.Do(func() {
+		instance = &Graceful{}
+		ctx, stop := signal.NotifyContext(ctxt,
+			syscall.SIGINT,  // interrupt: stopped by Ctrl + C
+			syscall.SIGTERM, // graceful kill
+		)
+		instance.NotifyContext = ctx
+		instance.stop = stop
+	})
+
+	return instance
+}
+func (g Graceful) Graceful(cleanup func()) {
+	s := <-g.NotifyContext.Done()
+	fmt.Println("Got signal:", s)
+	fmt.Println("\r- Ctrl+C or (kill -9) received")
+	cleanup()
+	g.stop()
 }
