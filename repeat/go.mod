14a78c04b3c8606c0c292c0de7726c51a10dac14
Gil Collins <50967180+colt3k@users.noreply.github.com>
2022-01-06 14:46:18 -0700

    add more logging using nglog

--- repeat/go.mod
+++ repeat/go.mod
@@ -1,3 +1,5 @@
 module github.com/colt3k/utils/repeat
 
 go 1.16
+
+require github.com/colt3k/nglog v0.0.19
--- repeat/repeat.go
+++ repeat/repeat.go
@@ -2,12 +2,13 @@ package repeat
 
 import (
 	"context"
-	"fmt"
 	"time"
+
+	log "github.com/colt3k/nglog/ng"
 )
 
 var (
-	loopId int64 = time.Now().Unix()
+	loopId int64 = 0
 )
 
 type Task func() error
@@ -31,15 +32,19 @@ func LoopId() int64 {
 	return loopId
 }
 func Process(ctx context.Context, r Rule, processName string, repeat Task, stop Task) error {
+	curTime := time.Now().Format(time.RFC3339)
+	log.Logln(log.DEBUG, "setting up repeat process as of %v", curTime)
 	var count uint = 0
 	if r.MaxAttempts == 0 {
-		fmt.Printf("%v - note: no max attempts has been set, this will continue forever\n", processName)
+		log.Logf(log.INFO, "%v - note: no max attempts has been set, this will continue forever", processName)
 	}
 	timer := time.NewTimer(r.InitialTimer)
 	for {
 		select {
 		case <-ctx.Done():
-			fmt.Printf("repeater cancelled: %v\n", processName)
+			timer.Stop()
+			log.Logf(log.INFO, "repeater cancelled: %v", processName)
+
 			if stop != nil {
 				if err := stop(); err != nil {
 					return err
@@ -47,19 +52,21 @@ func Process(ctx context.Context, r Rule, processName string, repeat Task, stop 
 			}
 			return nil
 		case t := <-timer.C:
+			// Stop the timer from being run again until our function finishes and returns
+			timer.Stop()
 			if r.MaxAttempts > 0 && count >= r.MaxAttempts {
-				timer.Stop()
 				return nil
 			}
+
 			loopId = time.Now().Unix()
-			fmt.Printf("timer fired: %v - %v\n", processName, t)
+			log.Logf(log.INFO, "%v timer fired: %v - %v", loopId, processName, t)
 			if err := repeat(); err != nil {
-				fmt.Errorf("(in repeater) exited task iteration with error %v", err)
+				log.Logf(log.ERROR, "(in repeater) %v exited task iteration with error %v\n", loopId, err)
 				return err
 			}
-			fmt.Printf("(in repeater) exited task iteration without error")
+			log.Logf(log.INFO, "(in repeater) %v exited task iteration without error\n", loopId)
 			count++
-			timer.Reset(r.RepeatTimer)
+			timer = time.NewTimer(r.RepeatTimer)
 		}
 	}
 }
