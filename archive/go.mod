dd0a8a36d9f15db96b02b0892bab5b7f94cbb2ef
Gil Collins <50967180+colt3k@users.noreply.github.com>
2022-06-03 13:16:53 -0700

    remove encode dependency

--- archive/go.mod
+++ archive/go.mod
@@ -3,10 +3,8 @@ module github.com/colt3k/utils/archive
 go 1.12
 
 require (
-	github.com/colt3k/utils/encode v0.0.2
+	github.com/frankban/quicktest v1.14.3 // indirect
 	github.com/pierrec/lz4 v2.6.0+incompatible
-	github.com/pkg/errors v0.8.1 // indirect
 	github.com/ulikunitz/xz v0.5.10
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8
-	golang.org/x/crypto v0.0.0-20190103213133-ff983b9c42bc // indirect
 )
--- archive/gz/test/main.go
+++ archive/gz/test/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"fmt"
 	"strings"
-
-	"github.com/colt3k/utils/encode"
-	"github.com/colt3k/utils/encode/encodeenum"
 
 	"github.com/colt3k/utils/archive/gz"
 )
@@ -14,16 +12,18 @@ import (
 func main() {
 
 	str := "just some regular text to compress and decompress, just some regular text to compress and decompress"
-	fmt.Println("Original: ",str)
+	fmt.Println("Original: ", str)
 
 	var byt bytes.Buffer
 	gz.Gz.Compress(strings.NewReader(str), &byt)
 
-	enc := encode.Encode(byt.Bytes(), encodeenum.B64STD)
-	fmt.Println("Compressed and Encoded: ",enc)
+	enc := base64.StdEncoding.EncodeToString(byt.Bytes())
+	fmt.Println("Compressed and Encoded: ", enc)
 
-	dec := encode.Decode([]byte(enc), encodeenum.B64STD)
-	fmt.Println("Compressed and Decoded: ",string(dec))
+	base64data := make([]byte, base64.StdEncoding.DecodedLen(len([]byte(enc))))
+	n, _ := base64.StdEncoding.Decode(base64data, []byte(enc))
+	dec := base64data[:n]
+	fmt.Println("Compressed and Decoded: ", string(dec))
 
 	var b2 bytes.Buffer
 	gz.Gz.Decompress(bytes.NewReader(dec), &b2)
